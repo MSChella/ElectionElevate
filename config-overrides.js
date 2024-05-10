601b67777a8a49b2b825d9aaac6507e4d56f7692
MSChella <reachchellapathy.ms@gmail.com>
2024-05-10 11:11:45 +0530

    Deployment Configs

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const { override } = require('customize-cra');
+
+module.exports = override(
+    (config) => {
+        // Add the babel plugin to the Babel configuration
+        const babelIndex = config.module.rules.findIndex(
+            (rule) => rule && rule.use && Array.isArray(rule.use)
+        );
+        if (babelIndex !== -1) {
+            const babelLoader = config.module.rules[babelIndex].use.find(
+                (use) => use && use.loader && use.loader.includes('babel-loader')
+            );
+            if (babelLoader) {
+                babelLoader.options.plugins.push(
+                    '@babel/plugin-proposal-private-property-in-object'
+                );
+            }
+        }
+        return config;
+    }
+);
